c162d50f4efcdfbbfa44de4a53ff71092d15239b
Zoë Davidson <zoe.a.h.davidson@gmail.com>
2021-07-06 20:49:29 -0400

    updated flexbox styling

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,23 +3,19 @@ import PlanetImage from "./PlanetImage";
 import PlanetInfo from "./PlanetInfo";
 
 const Hero = (props) => {
-  const planet = props.planet;
-  const setContent = props.setContent;
-  const content = props.content;
-  const image = props.image;
-  const setImage = props.setImage;
-  const geoImage = props.geoImage;
-  const setGeoImage = props.setGeoImage;
-
   return (
     <div className="hero">
-      <PlanetImage planet={planet} image={image} geoImage={geoImage} />
+      <PlanetImage
+        planet={props.planet}
+        image={props.image}
+        geoImage={props.geoImage}
+      />
       <PlanetInfo
-        planet={planet}
-        setContent={setContent}
-        content={content}
-        setImage={setImage}
-        setGeoImage={setGeoImage}
+        planet={props.planet}
+        setContent={props.setContent}
+        content={props.content}
+        setImage={props.setImage}
+        setGeoImage={props.setGeoImage}
       />
     </div>
   );
--- src/components/PlanetInfo.js
+++ src/components/PlanetInfo.js
@@ -1,32 +1,25 @@
 import "./PlanetInfo.scss";
 import InfoSelector from "./InfoSelector";
-import data from "../data.json";
 
 const PlanetInfo = (props) => {
   const planet = props.planet;
-  const setContent = props.setContent;
-  const content = props.content;
-  const setImage = props.setImage;
-  const setGeoImage = props.setGeoImage;
+
   return (
     <div className="planet-info">
       <div className="info-text">
-        <h1>{planet.name || data[0].name}</h1>
+        <h1>{planet.name}</h1>
         <p className="planet-description">
-          {content || planet.overview.content || data[0].overview.content}
+          {props.content || planet.overview.content}
         </p>
         <div className="planet-source">
-          Source:{" "}
-          <a href={planet.overview.source || data[0].overview.source}>
-            Wikipedia
-          </a>
+          Source: <a href={planet.overview.source}>Wikipedia</a>
         </div>
       </div>
       <InfoSelector
-        planet={planet}
-        setContent={setContent}
-        setImage={setImage}
-        setGeoImage={setGeoImage}
+        planet={props.planet}
+        setContent={props.setContent}
+        setImage={props.setImage}
+        setGeoImage={props.setGeoImage}
       />
     </div>
   );
