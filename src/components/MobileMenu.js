2eed44de4f795c076ac61fb1fff55205047eeb74
Zoë Davidson <zoe.a.h.davidson@gmail.com>
2021-07-08 07:56:49 -0400

    menu position adjust

--- src/components/MobileMenu.js
+++ src/components/MobileMenu.js
@@ -1,7 +1,18 @@
+import NavBar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
 import "./MobileMenu.scss";
 
 const MobileMenu = () => {
-  return <div className="mobile-menu"></div>;
+  return (
+    <div className="mobile-menu">
+      <h1>Your Menu</h1>
+      <ul>
+        <li>Lots</li>
+        <li>Of</li>
+        <li>Links</li>
+      </ul>
+    </div>
+  );
 };
 
 export default MobileMenu;
